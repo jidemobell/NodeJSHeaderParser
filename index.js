99e9ce4c9cc0bf689ff6fbc410b498d5fa1233f8
jide mobell <jidd32@gmail.com>
2017-11-10 21:54:42 +0100

    commit two

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const Address6 = require('ip-address').Address6;
-const reqIp = require('request-ip')
 
 const app = express();
 const port = process.env.PORT||8080
 app.use(bodyparser.urlencoded({extended:true}));
 
+
+
 app.get('/', (req,res)=>{
     let output = req.headers['user-agent']
    let lang = req.headers['accept-language']
-   let ip = new Address6(req.socket.address().address);
-   let ipv4 = ip.inspectTeredo();
+ 
 
-   var IP = req.headers['x-forwarded-for'] ||
+   var ip = req.headers['x-forwarded-for'].split(',')[0] ||
    req.connection.remoteAddress ||
    req.socket.remoteAddress ||
-req.connection.socket.remoteAddress;
+ req.connection.socket.remoteAddress;
 
-
+   console.log(ipMiddleware);
    let jsonOutput = {
-    ipaddress:IP,
+    ipaddress:ip,
     language: lang.substring(0,lang.lastIndexOf(',')),
     software: output.substring(output.lastIndexOf('(')+1,output.lastIndexOf(')'))
 }
 
 res.json(jsonOutput);
-console.log(req.headers['x-forwarded-for'])
+
 
 })
 
